62981aa44d123dcd77c2f888a0b462dbc9fea89f
halil.olcay <holcay92@gmail.com>
2022-12-08 02:08:10 +0300

    media player is improved . image added. play pause stop buttons are added. seek bar added but not working

--- app/src/main/java/com/example/clapapp/MainActivity.kt
+++ app/src/main/java/com/example/clapapp/MainActivity.kt
@@ -3,18 +3,34 @@ package com.example.clapapp
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     private var mediaPlayer : MediaPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mediaPlayer = MediaPlayer.create(this, R.raw.clapeffect)
+        mediaPlayer = MediaPlayer.create(this, R.raw.music)
 
-        val button = findViewById<android.widget.Button>(R.id.btnClap)
+      /*  val button = findViewById<android.widget.Button>(R.id.btnClap)
         button.setOnClickListener {
             mediaPlayer?.start()
             println("Clap")
+        }*/
+
+        val btn_play = findViewById<FloatingActionButton>(R.id.btn_play)
+        val btn_pause = findViewById<FloatingActionButton>(R.id.btn_pause)
+        val btn_stop = findViewById<FloatingActionButton>(R.id.btn_stop)
+        btn_play.setOnClickListener {
+            mediaPlayer?.start()
+        }
+        btn_pause.setOnClickListener {
+            mediaPlayer?.pause()
+        }
+        btn_stop.setOnClickListener {
+            mediaPlayer?.stop()
+            // initialize the media player again
+            mediaPlayer = MediaPlayer.create(this, R.raw.music)
         }
     }
 }
